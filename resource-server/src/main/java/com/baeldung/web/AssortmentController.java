2db594fc155e1157dd9a59fea3431fdd62fd4f55
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2023-07-13 14:31:24 +0200

    modified JWT config to contain roles of resource owner

--- resource-server/src/main/java/com/baeldung/web/AssortmentController.java
+++ resource-server/src/main/java/com/baeldung/web/AssortmentController.java
@@ -3,6 +3,8 @@ package com.baeldung.web;
 import eu.kartoffelquadrat.bookstoreinternals.AssortmentImpl;
 import eu.kartoffelquadrat.bookstoreinternals.BookDetailsImpl;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
@@ -13,19 +15,24 @@ import java.util.Collection;
 @CrossOrigin
 public class AssortmentController {
 
-    @GetMapping("/bookstore/isbns")
-    public Collection<Long> getEntireAssortment() {
-        return AssortmentImpl.getInstance().getEntireAssortment();
-    }
+  @GetMapping("/bookstore/isbns")
+  public Collection<Long> getEntireAssortment() {
+    return AssortmentImpl.getInstance().getEntireAssortment();
+  }
 
-    @GetMapping("/bookstore/isbns/{isbn}")
-    public BookDetailsImpl getBookDetails(@PathVariable("isbn") Long isbn) {
-        return AssortmentImpl.getInstance().getBookDetails(isbn);
-    }
+  @GetMapping("/bookstore/isbns/{isbn}")
+  public BookDetailsImpl getBookDetails(@PathVariable("isbn") Long isbn) {
+    return AssortmentImpl.getInstance().getBookDetails(isbn);
+  }
 
-//    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @PutMapping("/bookstore/isbns/{isbn}")
-    public void addBookToAssortment(@RequestBody BookDetailsImpl bookDetails) {
-        AssortmentImpl.getInstance().addBookToAssortment(bookDetails);
-    }
+  // TODO: Figure out if there is a way to check the ROLE, not the NAME.
+  //    @PreAuthorize("hasRole('ROLE_ADMIN')")
+//  @PreAuthorize("authentication.name == 'AssortmentExtender'") // <-- works but is a hack.
+//  @PreAuthorize("authentication.authorities.contains('ROLE_ADMIN')")
+  @PutMapping("/bookstore/isbns/{isbn}")
+  public void addBookToAssortment(@RequestBody BookDetailsImpl bookDetails, final @AuthenticationPrincipal
+  Jwt jwt) {
+    System.out.println("\nDEBUG TOKEN INFO:\n"+jwt.getTokenValue());
+    AssortmentImpl.getInstance().addBookToAssortment(bookDetails);
+  }
 }
--- spring-authorization-server/src/main/java/com/baeldung/config/DefaultSecurityConfig.java
+++ spring-authorization-server/src/main/java/com/baeldung/config/DefaultSecurityConfig.java
@@ -1,11 +1,19 @@
 package com.baeldung.config;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.core.OAuth2TokenType;
+import org.springframework.security.oauth2.server.authorization.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenCustomizer;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,11 +24,28 @@ public class DefaultSecurityConfig {
 
   @Bean
   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-    http.authorizeRequests(authorizeRequests ->
-            authorizeRequests.anyRequest().authenticated()
-        )
+    http.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
         .formLogin(withDefaults());
     return http.build();
+  }
+
+  /**
+   * This bean extends the default content of JWTs by an additional ROLE field, which is needed to
+   * restrict access on ADMIN endpoints by proxy services invoked by ordinary users.
+   * See: https://stackoverflow.com/a/68723813/13805480
+   * See: https://www.appsdeveloperblog.com/add-roles-to-jwt-issued-by-new-spring-authorization-server/
+   * @return
+   */
+  @Bean
+  OAuth2TokenCustomizer<JwtEncodingContext> jwtCustomizer() {
+    return context -> {
+      if (context.getTokenType() == OAuth2TokenType.ACCESS_TOKEN) {
+        Authentication principal = context.getPrincipal();
+        Set<String> authorities = principal.getAuthorities().stream().map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toSet());
+        context.getClaims().claim("roles", authorities);
+      }
+    };
   }
 
   @Bean
@@ -29,36 +54,27 @@ public class DefaultSecurityConfig {
     // TODO: add user for assortment (write) access.
     // The database knows several users: 2 generic users for comments and catalogue,
     // several for the individual stock locactions
-    UserDetails assortmentExtender = User.withDefaultPasswordEncoder()
-        .username("AssortmentExtender")
-        .password("password")
-        .roles("ADMIN")
-        .build();
+    UserDetails assortmentExtender =
+        User.withDefaultPasswordEncoder().username("AssortmentExtender").password("password")
+            .roles("ADMIN").build();
 
-    UserDetails montrealStoreManager = User.withDefaultPasswordEncoder()
-        .username("Montréal")
-        .password("password")
-        .roles("USER")
-        .build();
+    UserDetails montrealStoreManager =
+        User.withDefaultPasswordEncoder().username("Montréal").password("password").roles("USER")
+            .build();
 
-    UserDetails munchenStoreManager = User.withDefaultPasswordEncoder()
-        .username("München")
-        .password("password")
-        .roles("USER")
-        .build();
+    UserDetails munchenStoreManager =
+        User.withDefaultPasswordEncoder().username("München").password("password").roles("USER")
+            .build();
 
-    UserDetails osterhofenStoreManager = User.withDefaultPasswordEncoder()
-        .username("Osterhofen")
-        .password("password")
-        .roles("USER")
-        .build();
+    UserDetails osterhofenStoreManager =
+        User.withDefaultPasswordEncoder().username("Osterhofen").password("password").roles("USER")
+            .build();
 
-    UserDetails lyonStoreManager = User.withDefaultPasswordEncoder()
-        .username("Lyon")
-        .password("password")
-        .roles("USER")
-        .build();
-    return new InMemoryUserDetailsManager(assortmentExtender, montrealStoreManager, munchenStoreManager, osterhofenStoreManager, lyonStoreManager);
+    UserDetails lyonStoreManager =
+        User.withDefaultPasswordEncoder().username("Lyon").password("password").roles("USER")
+            .build();
+    return new InMemoryUserDetailsManager(assortmentExtender, montrealStoreManager,
+        munchenStoreManager, osterhofenStoreManager, lyonStoreManager);
   }
 
 }
