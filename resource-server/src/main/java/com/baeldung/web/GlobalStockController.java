32aba6b3b46fadfaa8bfc722b16b177bc8e30e60
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2023-07-27 10:06:34 -0400

    Added second SpEL test to search java collection

--- resource-server/src/main/java/com/baeldung/web/GlobalStockController.java
+++ resource-server/src/main/java/com/baeldung/web/GlobalStockController.java
@@ -15,32 +15,36 @@ import java.util.Map;
 public class GlobalStockController {
 
 
-    @GetMapping("/bookstore/stocklocations/{stocklocation}/{isbn}")
-    public int getStock(@PathVariable("stocklocation") String city, @PathVariable("isbn") Long isbn) {
+  @GetMapping("/bookstore/stocklocations/{stocklocation}/{isbn}")
+  public int getStock(@PathVariable("stocklocation") String city, @PathVariable("isbn") Long isbn) {
 
-        return GlobalStockImpl.getInstance().getStock(city, isbn);
-    }
+    return GlobalStockImpl.getInstance().getStock(city, isbn);
+  }
 
-    // Old PreAuthorize, that compares account name to city, string match.
-    //    @PreAuthorize("authentication.name == #city")
-    // Now we're interested in allowing accounts that are associated to city by domain model.
-    // See PreAuthorize syntax documentation: https://docs.spring.io/spring-security/site/docs/3.0.x/reference/el-access.html
-    // TODO: Write SpEL that resolves city localstock object and traverses to employee list.
-    // Possibly I can get the singleton like so: https://stackoverflow.com/q/7585627/13805480
-    @PreAuthorize("T(eu.kartoffelquadrat.bookstoreinternals.GlobalStockImpl).getInstance().isEmployed(#city, authentication.name)")
-    @PostMapping("/bookstore/stocklocations/{stocklocation}/{isbn}")
-    public void setStock(@PathVariable("stocklocation") String city, @PathVariable("isbn") Long isbn, @RequestBody Integer amount) {
+  // Old PreAuthorize, that compares account name to city, string match.
+  //    @PreAuthorize("authentication.name == #city")
+  // Now we're interested in allowing accounts that are associated to city by domain model.
+  // See PreAuthorize syntax documentation: https://docs.spring.io/spring-security/site/docs/3.0.x/reference/el-access.html
+  // TODO: Write SpEL that resolves city localstock object and traverses to employee list.
+  // Possibly I can get the singleton like so: https://stackoverflow.com/q/7585627/13805480
+  // @PreAuthorize("T(eu.kartoffelquadrat.bookstoreinternals.GlobalStockImpl).getInstance().isEmployed(#city, authentication.name)")
 
-        GlobalStockImpl.getInstance().setStock(city, isbn, amount);
-    }
+  // Alternative access method using manual inspection of employee list.
+  @PreAuthorize("T(eu.kartoffelquadrat.bookstoreinternals.GlobalStockImpl).getInstance().getAllEmployeesForStore(#city).contains(authentication.name)")
+  @PostMapping("/bookstore/stocklocations/{stocklocation}/{isbn}")
+  public void setStock(@PathVariable("stocklocation") String city, @PathVariable("isbn") Long isbn,
+                       @RequestBody Integer amount) {
 
-    @GetMapping("/bookstore/stocklocations")
-    public Collection<String> getStoreLocations() {
-        return GlobalStockImpl.getInstance().getStoreLocations();
-    }
+    GlobalStockImpl.getInstance().setStock(city, isbn, amount);
+  }
 
-    @GetMapping("/bookstore/stocklocations/{stocklocation}")
-    public Map<Long, Integer> getEntireStoreStock(@PathVariable("stocklocation") String city) {
-        return GlobalStockImpl.getInstance().getEntireStoreStock(city);
-    }
+  @GetMapping("/bookstore/stocklocations")
+  public Collection<String> getStoreLocations() {
+    return GlobalStockImpl.getInstance().getStoreLocations();
+  }
+
+  @GetMapping("/bookstore/stocklocations/{stocklocation}")
+  public Map<Long, Integer> getEntireStoreStock(@PathVariable("stocklocation") String city) {
+    return GlobalStockImpl.getInstance().getEntireStoreStock(city);
+  }
 }
